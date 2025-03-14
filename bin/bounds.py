741d8196fda7b3f6dd141b6e0e6e8bb3610c01d4
Darren Embry <dse@webonastick.com>
2025-03-14 15:34:21 -0400

    move bounds checking to own program

--- bin/bounds.py
+++ bin/bounds.py
@@ -7,36 +7,25 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(__file__) + "/../lib")
-from my_font_utils import parse_glyph_svg_filename
-from my_font_utils import reconstitute_references
-from my_font_utils import import_svg_glyph
-from my_font_utils import create_smol_glyph
 from my_font_utils import check_all_glyph_bounds
 
-sys.path.append(os.getenv("HOME") + "/git/dse.d/fontforge-utilities/lib")
-import mixedjsontext
-
 DEFAULT_WIDTH = 1008
-STROKE_WIDTH_BASIS = 96
 LATIN_SMALL_LETTER_SCHWA = 0x0259
 
 def main():
     global args
     parser = argparse.ArgumentParser()
     parser.add_argument('font_filename', help="font filename")
-    parser.add_argument('svg_filenames', nargs='+', help="svg characters")
     parser.add_argument('-w', '--width', type=int, default=DEFAULT_WIDTH)
-    parser.add_argument('-o', '--save-as', '--output', type=str,
-                        help="after editing, save as new file, converts if file extension is different")
+    parser.add_argument('-o', '--save-as', '--output', type=str)
     parser.add_argument('--verbose', '-v', action='count', default=0)
     args = parser.parse_args()
 
     font = fontforge.open(args.font_filename)
     write_font_filename = args.save_as if args.save_as is not None else args.font_filename
 
-    print("Importing glyphs...")
-    for svg_filename in args.svg_filenames:
-        import_svg_glyph(font, svg_filename, args.width)
+    print("Checking glyph bounds...")
+    check_all_glyph_bounds(font, args.width)
 
     print("Writing %s" % write_font_filename)
     if write_font_filename.endswith('.sfd'):
