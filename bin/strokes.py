0b4a62e1d1c00c179b908d0d28e8fb4c43233107
Darren Embry <dse@webonastick.com>
2025-03-20 19:21:18 -0400

    speed up some of the letters for expanding strokes

--- bin/strokes.py
+++ bin/strokes.py
@@ -6,6 +6,9 @@ import argparse
 import os
 import sys
 import statistics
+import time
+import re
+import unicodedata
 
 sys.path.append(os.getenv("HOME") + "/git/dse.d/fontforge-utilities/lib")
 import mixedjsontext
@@ -22,6 +25,7 @@ def main():
     parser.add_argument('-o', '--save-as', '--output', type=str,
                         help="after editing, save as new file, converts if file extension is different")
     parser.add_argument('--verbose', '-v', action='count', default=0)
+    parser.add_argument('--log', action='store_true')
     args = parser.parse_args()
 
     font = fontforge.open(args.font_filename)
@@ -30,7 +34,8 @@ def main():
     # MONOSPACE
     common_glyph_width = statistics.mode([glyph.width for glyph in font.glyphs()])
 
-    silence.on()
+    fh = open('strokes.log', 'w', encoding='utf-8') if args.log else None
+
     for glyph in font.glyphs():
         print("strokes.py %s: Expanding strokes on %s %s" % (args.font_filename, u(glyph.unicode), glyph.glyphname))
         if glyph.unicode in range(0x2500, 0x25a0):
@@ -45,12 +50,15 @@ def main():
             continue
         orig_width = glyph.width
         if args.expand_stroke is not None:
+            time_start = time.time()
             glyph.stroke("circular", args.expand_stroke)
+            time_end = time.time()
+            if fh is not None:
+                fh.write("%8.6f  %s\n" % ((time_end - time_start), charname(glyph)))
         if orig_width != 0:
             glyph.width = orig_width
         else:
             glyph.width = common_glyph_width # MONOSPACE
-    silence.off()
 
     if write_font_filename.endswith('.sfd'):
         print("strokes.py %s: Saving %s..." % (args.font_filename, write_font_filename))
@@ -58,5 +66,34 @@ def main():
     else:
         print("strokes.py %s: Generating %s..." % (args.font_filename, write_font_filename))
         font.generate(write_font_filename)
+
+    if fh is not None:
+        fh.close()
+
+def charname(glyph):
+    if glyph.unicode >= 0:
+        try:
+            unicode_name = unicodedata.name(chr(glyph.unicode))
+            return "%s '%s' %s" % (u(glyph.unicode), glyph.glyphname, unicode_name)
+        except ValueError:
+            return "%s '%s' (%s)" % (u(glyph.unicode), glyph.glyphname, "(no name)")
+    idx = glyph.glyphname.find('.')
+    if idx < 1:                 # no "." or starts with "."
+        return "-1 '%s'" % glyph.glyphname
+    real_glyphname = glyph.glyphname[0:idx]
+    try:
+        real_codepoint = fontforge.unicodeFromName(real_glyphname)
+    except ValueError:
+        return "-1 '%s'" % glyph.glyphname
+    try:
+        real_unicode_name = unicodedata.name(chr(real_codepoint))
+    except ValueError:
+        real_unicode_name = "(no name)"
+    return "-1 '%s' %s %s" % (glyph.glyphname, u(real_codepoint), real_unicode_name)
+
+def u(codepoint):
+    if codepoint < 0:
+        return "%d" % codepoint
+    return "U+%04X" % codepoint
 
 main()        
