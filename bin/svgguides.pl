da0f1615097678497f3a6e45f0b88f7c404c2a73
Darren Embry <dse@webonastick.com>
2025-03-30 21:39:59 -0400

    restore guides

--- bin/svgguides.pl
+++ bin/svgguides.pl
@@ -4,23 +4,41 @@ use strict;
 use XML::LibXML;
 use XML::LibXML::XPathContext;
 use Getopt::Long;
-use Carp::Always;
+# use Carp::Always;
+
+use constant COLOR_GREEN    => '#009900';
+use constant COLOR_GREEN_20 => '#ff9900';
+use constant COLOR_BLACK    => '#000000';
+use constant COLOR_BLUE     => '#0000ff';
+use constant COLOR_RED      => '#ff0000';
+
+use constant COLOR_IS_BROWN => '#986a44';
+use constant COLOR_IS_BLUE  => '#0099e5'; # 26, 4d
+use constant COLOR_IS_RED   => '#ff0000'; # 7f
+
+use constant COLOR_GRID_BLUE           => '#b3b3ff';
+use constant COLOR_GRID_GREEN          => '#5aff5a';
+use constant COLOR_GRID_RED            => '#ff9e9e';
+use constant COLOR_GRID_GRAY           => '#bbbbbb';
+use constant COLOR_GRID_ORANGE         => '#ffab57';
+use constant COLOR_GRID_MAGENTA        => '#ff8cff';
+use constant COLOR_GRID_CYAN           => '#1cffff';
+use constant COLOR_GRID_YELLOW         => '#ffff00'; # higher luminance
+use constant COLOR_GRID_BLACK          => '#000000';
+use constant COLOR_GRID_WHITE          => '#ffffff';
+use constant COLOR_GRID_NON_REPRO_BLUE => '#95c9d7';
+
+use constant COLOR_OVERSHOOT_GREEN => '#8ff0a4';
+use constant COLOR_EXCENTER_RED => '#bf4040';
+use constant COLOR_CAPCENTER_BLACK => '#000000';
 
 our %NS;
-our $GREEN;
-our $BLUE;
-our $BLACK;
-our $BROWN;
 BEGIN {
     %NS = (
         inkscape => 'http://www.inkscape.org/namespaces/inkscape',
         sodipodi => 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
         svg      => 'http://www.w3.org/2000/svg',
     );
-    $GREEN = "rgb(143,240,164)";
-    $BLUE = "rgb(0,134,229)";
-    $BLACK = "rgb(0,0,0)";
-    $BROWN = "rgb(152,106,68)";
 }
 
 our $in_place = 0;
@@ -34,13 +52,43 @@ while (<>) {
     printf STDERR ("Read $ARGV\n");
     $thingy = My::Thingy->new();
     $thingy->load_xml($_);
-    $thingy->move_or_create_horizontal_guide('accentcenter', 1536);
-    $thingy->move_or_create_horizontal_guide('accentbelowcenter', 192);
+    $thingy->delete_guides();
 
-    $thingy->move_or_create_horizontal_guide('descender', 36);
-    $thingy->move_or_create_horizontal_guide(undef, 84);
-    $thingy->move_or_create_horizontal_guide(undef, 64, $GREEN);
-    $thingy->move_or_create_horizontal_guide(undef, 132);
+    $thingy->create_guide(16, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(64, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(112, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(36, name => 'descender');
+    $thingy->create_guide(84);
+    $thingy->create_guide(132);
+
+    $thingy->create_guide(316, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(364, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(412, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(336, name => 'baseline');
+    $thingy->create_guide(384);
+    $thingy->create_guide(432);
+
+    $thingy->create_guide(864, color => COLOR_CAPCENTER_BLACK, name => 'cap-center');
+
+    $thingy->create_guide(1092, name => 'ex-height');
+    $thingy->create_guide(1044);
+    $thingy->create_guide(996);
+    $thingy->create_guide(1112, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(1064, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(1016, color => COLOR_OVERSHOOT_GREEN);
+
+    $thingy->create_guide(1392, name => 'ascender');
+    $thingy->create_guide(1344);
+    $thingy->create_guide(1296);
+    $thingy->create_guide(1412, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(1364, color => COLOR_OVERSHOOT_GREEN);
+    $thingy->create_guide(1316, color => COLOR_OVERSHOOT_GREEN);
+
+    $thingy->create_guide(714, color => COLOR_EXCENTER_RED, name => 'ex-center/oper-center');
+
+    $thingy->create_guide(504, orientation => 'vertical');
+
+    $thingy->create_guide(1536, name => 'accent-center');
 } continue {
     if (eof && $in_place && $ARGV ne '-') {
         my $fh;
@@ -86,12 +134,7 @@ package My::Thingy {
         $self->{view_box_ymin} = $view_box_ymin;
         $self->{view_box_ymax} = $view_box_ymax;
     }
-    sub move_or_create_horizontal_guide {
-        my ($self, $guide_name, $new_pos_y, $color) = @_;
-        $self->move_horizontal_guide($guide_name, $new_pos_y, $color) or
-          $self->create_horizontal_guide($guide_name, $new_pos_y, $color);
-    }
-    sub list_horizontal_guides {
+    sub list_guides {
         my ($self) = @_;
         foreach my $guide ($self->{xpc}->findnodes("//sodipodi:guide")) {
             my $pos = $guide->getAttribute("position");
@@ -101,54 +144,44 @@ package My::Thingy {
             print("$pos_x, $pos_y => $pos_x_svg, $pos_y_svg\n");
         }
     }
-    sub move_horizontal_guide {
-        my ($self, $guide_name, $new_pos_y_svg, $color) = @_;
-        my $new_pos_y_vbox = $self->y_svg_to_view_box($new_pos_y_svg);
-        my $guide;
-        if (defined $guide_name) {
-            ($guide) = $self->{xpc}->findnodes("//sodipodi:guide[\@inkscape:label='$guide_name']");
-        } else {
-            my @guides = $self->{xpc}->findnodes("//sodipodi:guide");
-            foreach my $g (@guides) {
-                my $pos = $g->getAttribute("position");
-                my ($pos_x_vbox, $pos_y_vbox) = split(/\s*,\s*/, $pos);
-                if (abs($pos_x_vbox - 0) < 0.001 && abs($pos_y_vbox - $new_pos_y_vbox) < 0.001) {
-                    $guide = $g;
-                    last;
-                }
-            }
+    sub delete_guides {
+        my ($self) = @_;
+        foreach my $guide ($self->{xpc}->findnodes("//sodipodi:guide")) {
+            $guide->parentNode->removeChild($guide);
         }
-        if (!$guide) {
-            return;
-        }
-        my $position = $guide->getAttribute('position');
-        my ($pos_x_vbox, $pos_y_vbox) = split(/\s*,\s*/, $position);
-        my $orientation = $guide->getAttribute('orientation');
-        my ($orientn_x, $orientn_y) = split(/\s*,\s*/, $orientation);
-        my $is_horizontal_guide = !$orientn_x && $orientn_y; # 0,1
-        my $is_vertical_guide = $orientn_x && !$orientn_y;   # 1,0
-        my $height = $self->{height};
-        my $width = $self->{width};
-        if ($is_horizontal_guide) {
-            $guide->setAttribute('position', "${pos_x_vbox},${new_pos_y_vbox}");
-            if (defined $color) {
-                $guide->setAttribute("inkscape:color", $color);
-            }
-        }
-        return 1;
     }
-    sub create_horizontal_guide {
-        my ($self, $guide_name, $new_pos_y_svg, $color) = @_;
-        $color //= $BLUE;
-        my $new_pos_y_vbox = $self->y_svg_to_view_box($new_pos_y_svg);
+    sub create_guide {
+        my ($self, $new_pos_svg, %args) = @_;
+        my $color       = $args{color};
+        my $guide_name  = $args{name};
+        my $orientation = $args{orientation} // "horizontal";
+        $orientation = "horizontal" if lc(substr($orientation, 0, 1)) eq 'h';
+        $orientation = "vertical" if lc(substr($orientation, 0, 1)) eq 'v';
+        my $is_horizontal = $orientation eq 'horizontal';
         my $guide = $self->{doc}->createElement("sodipodi:guide");
-        $guide->setAttribute('position', sprintf("%f,%f", 0, $new_pos_y_vbox));
-        $guide->setAttribute("inkscape:locked", "false");
+        if ($is_horizontal) {
+            my $new_pos_vbox = $self->y_svg_to_view_box($new_pos_svg);
+            $guide->setAttribute('position', sprintf("%f,%f", 0, $new_pos_vbox));
+        } else {
+            my $new_pos_vbox = $self->x_svg_to_view_box($new_pos_svg);
+            $guide->setAttribute('position', sprintf("%f,%f", $new_pos_vbox, 0));
+        }
+        if ($args{locked}) {
+            $guide->setAttribute("inkscape:locked", "true");
+        } else {
+            $guide->setAttribute("inkscape:locked", "false");
+        }
         if (defined $guide_name) {
             $guide->setAttribute("inkscape:label", $guide_name);
         }
-        $guide->setAttribute("inkscape:color", $color);
-        $guide->setAttribute("orientation", "0,1");
+        if (defined $color) {
+            $guide->setAttribute("inkscape:color", $color);
+        }
+        if ($is_horizontal) {
+            $guide->setAttribute("orientation", "0,1");
+        } else {
+            $guide->setAttribute("orientation", "1,0");
+        }
         my ($namedview) = $self->{xpc}->findnodes("//sodipodi:namedview");
         if (!$namedview) {
             die("no namedview element\n");
