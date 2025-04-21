d24fb89f47c41fd25618be7aaab24498205b3d17
Darren Embry <dse@webonastick.com>
2025-04-20 23:18:47 -0400

    specimen customizations

--- specimen/src/_data/site.js
+++ specimen/src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "Specimen Builder", // Edit this with the name of your type specimen
+	title: "Reproducing Typewriter", // Edit this with the name of your type specimen
 
 	// This determines the reading direction of the text blocks and glyph table.
 	// Options:
@@ -10,16 +10,15 @@ module.exports = {
 	direction: "ltr",
 
 	description:
-		"A design built on top of Specimen Skeleton, based on research insights into effective digital type specimens.", // The description of your type specimen.
+		"A typewriter typeface from the early 20th century designed for better visibility.", // The description of your type specimen.
 
-	typeface: "STIX fonts", // The name of the typeface you are building
-	link: "https://www.stixfonts.org", // This is the URL used for the button
+	typeface: "Reproducing Typewriter", // The name of the typeface you are building
+	link: "https://www.example.com/", // This is the URL used for the button
 	about:
-		"The mission of the Scientific and Technical Information Exchange (STIX) font creation project is the preparation of a comprehensive set of fonts that serve the scientific and engineering community in the process from manuscript creation through final publication, both in electronic and print formats.", // All about the typeface
-	designers: "Tiro Typeworks", // And who designed it
+		"A typewriter typeface from the early 20th century designed for better visibility.",
+	designers: "Darren Embry", // And who designed it
 	coverage: "Latin, Greek, & Cyrillic", // This populates the link that anchors to the list of languages.
-	language:
-		"Afrikaans, Albanian, Asturian, Basque, Bosnian, Breton, Catalan, Cebuano, Cornish, Corsican, Croatian, Czech, Danish, English, Esperanto, Estonian, Faroese, Faroese, Filipino, Finnish, Flemish, French, Frisian, Friulian, Gaelic, Galician, German, Greenlandic, Guarani, Haitian, Hawaiian, Hiligaynon, Hungarian, Icelandic, Igbo, Indonesian, Irish, Italian, Kurdish, Latin, Latvian, Lithuanian, Livonian, Luxembourgish, Malagasy, Malay, Maltese, Maori, Moldavian, Nederlands, Norwegian, Occitan, Polish, Portuguese, Provencal, Romanian, Romansch, Saami, Samoan, Scots, Scottish, Slovak, Slovenian, Spanish, Swahili, Swedish, Tagalog, Turkish, Walloon, Welsh, Wolof, Belarusian, Bulgarian, Macedonian, Moldavian, Russian, Serbian (Cyrillic), Ukrainian, Greek.",
+	language: "Many Latin, Greek, & Cyrillic languages",
 
 	// More info: https://css-tricks.com/essential-meta-tags-social-media/
 	metatags: [
