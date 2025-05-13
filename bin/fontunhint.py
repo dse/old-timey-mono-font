f4e074f1a3c773e7d1ca21c576c6e21287bc3fe2
Darren Embry <dse@webonastick.com>
2025-05-13 18:18:32 -0400

    script to remove hints

---
+++ bin/fontunhint.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S fontforge -quiet
+# -*- mode: python; coding: utf-8 -*-
+import fontforge, argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('filenames', nargs='+')
+args = parser.parse_args()
+for filename in args.filenames:
+    font = fontforge.open(filename)
+    for glyph in font.glyphs():
+        glyph.dhints = ()
+        glyph.hhints = ()
+        glyph.vhints = ()
+    if filename.endswith(".sfd"):
+        font.save(filename)
+    else:
+        font.generate(filename)
+    font.close()
