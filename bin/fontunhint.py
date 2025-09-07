5f05440fe21b989150b8214c91bea5a2a8ba656e
Darren Embry <dse@webonastick.com>
2025-09-07 19:57:26 -0400

    merge the two fontunhint programs.

--- bin/fontunhint.py
+++ bin/fontunhint.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env -S fontforge -quiet
 # -*- mode: python; coding: utf-8 -*-
-import fontforge, argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('filenames', nargs='+')
-args = parser.parse_args()
-for filename in args.filenames:
-    font = fontforge.open(filename)
-    for glyph in font.glyphs():
-        glyph.dhints = ()
-        glyph.hhints = ()
-        glyph.vhints = ()
-    if filename.endswith(".sfd"):
-        font.save(filename)
-    else:
-        font.generate(filename)
-    font.close()
+import fontforge, argparse, os
+def main():
+    global args
+    parser = argparse.ArgumentParser(description="remove hints from specified font(s)")
+    parser.add_argument('filename', nargs='+')
+    args = parser.parse_args()
+    for filename in args.filename:
+        font = fontforge.open(args.source_filename)
+        for glyph in font.glyphs():
+            # glyph.dhints = tuple()
+            glyph.hhints = tuple()
+            glyph.vhints = tuple()
+            glyph.manualHints = True
+            font.gasp_version = 1
+            font.gasp = ((1, ('antialias',),),)
+        if filename.endswith(".sfd"):
+            font.save(filename)
+        else:
+            font.generate(filename, flags=['no-hints','no-flex'])
+        font.close()
