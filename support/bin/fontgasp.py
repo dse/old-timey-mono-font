2f6127502aad3b3e296e7d950aa1bac8862b15b7
Darren Embry <dse@webonastick.com>
2026-02-06 18:23:56 -0500

    gasp for normal fonts

---
+++ support/bin/fontgasp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S fontforge -quiet
+# -*- mode: python; coding: utf-8 -*-
+import os
+import fontforge
+import argparse
+def main():
+    global args
+    parser.add_argument('filenames', nargs='+')
+    args = parser.parse_args()
+    for filename in args.filenames:
+        font = fontforge.open(filename)
+        for glyph in font.glyphs():
+            glyph.dhints = tuple()
+            glyph.hhints = tuple()
+            glyph.vhints = tuple()
+            glyph.manualHints = True
+        font.gasp_version = 1
+        font.gasp = (
+            (10,    ('antialias',          'symmetric-smoothing',                    ),),
+            (65535, ('antialias',          'symmetric-smoothing',                    ),),
+        )
+        if filename.endswith(".sfd"):
+            font.save(filename)
+        else:
+            font.generate(filename)
+        font.close()
+main()
