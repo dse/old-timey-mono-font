d2817dabcae1382d7d424a90463cdc033ef1eddd
Darren Embry <dse@webonastick.com>
2026-03-29 00:47:29 -0400

    updates for chug

--- website/v1/gulpfile.js
+++ website/v1/gulpfile.js
@@ -1,131 +1,43 @@
-/*global fontData, fontGlyphsData */
+import {
+    buildTask, devTask,
+    sassTask, cleanTask, initTask,
+    nunjucksTask, rollupTask, staticTask,
+    startServerTask,
+    watchTask,
+    buildSeriesTask,
+    devSeriesTask,
+} from "../../chug/index.js";
 
-import gulp from "gulp";
-import * as sassImpl from "sass";
-import gulpSass from "gulp-sass";
-import nunjucks from "gulp-nunjucks-render";
-import beautify from "gulp-beautify";
-import data from "gulp-data";
-import { Transform } from "node:stream";
-import browserSync from "browser-sync";
+buildSeriesTask.displayName = "build:series";
+devSeriesTask.displayName = "dev:series";
 
-import getGlyphLists from "./src/js/glyph-lists.js";
-
-const htmlData = {
-    glyphLists: getGlyphLists(),
+export {
+    buildTask as build,
+    devTask as dev,
+    cleanTask as clean,
+    initTask as init,
+    sassTask as sass,
+    nunjucksTask as nunjucks,
+    rollupTask as rollup,
+    staticTask as static,
+    startServerTask as server,
+    watchTask as watch,
+    buildSeriesTask as buildSeries,
+    devSeriesTask as devSeries,
 };
 
-const sass = gulpSass(sassImpl);
-
-const EXCLUDE_PARTIALS = [
-    "!**/_*",
-    "!**/_*/**/*",
-];
-const EXCLUDE_TEMP = [
-    "!**/*.tmp",
-    "!**/*~",
-    "!**/#*#",
-];
-const COMPILE_EXCLUDE = [
-    ...EXCLUDE_PARTIALS,
-    ...EXCLUDE_TEMP,
-];
-const WATCH_EXCLUDE = [
-    ...EXCLUDE_TEMP,
-];
-
-function wroteWriter() {
-    return new Transform({
-        objectMode: true,
-        transform(file, enc, callback) {
-            console.log(`Wrote ${file.path}`);
-            callback(null, this);
-        },
-    });
-}
-
-function sassTask() {
-    return gulp.src(["src/styles/*.scss", ...COMPILE_EXCLUDE])
-               .pipe(sass({
-                   quietDeps: true,
-                   silenceDeprecations: [
-                       "import",
-                   ],
-               }))
-               .pipe(gulp.dest("dist/css"))
-               .pipe(wroteWriter());
-}
-
-let resetHtmlLastRunFlag = true;
-
-function htmlTask() {
-    const since = resetHtmlLastRunFlag ? {} : { since: gulp.lastRun(htmlTask) };
-    resetHtmlLastRunFlag = false;
-    return gulp.src(["src/html/**/*.njk", ...COMPILE_EXCLUDE], since)
-               .pipe(data(_ => htmlData))
-               .pipe(nunjucks({ path: "src/html" }))
-               .pipe(beautify.html())
-               .pipe(gulp.dest("dist"))
-               .pipe(wroteWriter());
-}
-
-function resetHtmlLastRun(cb) {
-    resetHtmlLastRunFlag = true;
-    cb?.();
-}
-
-let server;
-
-function startTask(cb) {
-    if (server) {
-        cb?.();
-        return;
-    }
-    server = browserSync.create();
-    server.init({
-        server: "./dist",
-    }, cb);
-}
-
-function reloadTask(cb) {
-    if (server) {
-        server.reload();
-    }
-    cb?.();
-}
-
-function watchTask() {
-
-    const watchSass = ["src/styles/**/*",
-                       ...WATCH_EXCLUDE];
-    const watchHtml = ["src/html/**/*",
-                       ...EXCLUDE_PARTIALS,
-                       ...WATCH_EXCLUDE];
-    const watchHtmlPartials = ["src/html/**/_*",
-                               "src/html/**/_*/**/*",
-                               ...WATCH_EXCLUDE];
-
-    console.log(`Watching ${JSON.stringify(watchSass)}`);
-    gulp.watch(watchSass, gulp.series(sassTask, reloadTask));
-    console.log(`Watching ${JSON.stringify(watchHtml)}`);
-    gulp.watch(watchHtml, gulp.series(htmlTask, reloadTask));
-    console.log(`Watching ${JSON.stringify(watchHtmlPartials)}`);
-    gulp.watch(watchHtmlPartials, gulp.series(resetHtmlLastRun, htmlTask, reloadTask));
-}
-
-const buildTask = gulp.parallel(
-    sassTask,
-    htmlTask,
-);
-
-const devTask = gulp.series(
-    buildTask,
-    startTask,
-    watchTask,
-);
-
-export { devTask as dev };
-export { buildTask as build };
-export { sassTask as sass };
-export { htmlTask as html };
-export default buildTask;
+// export const exports = {
+//     build:          buildTask,
+//     dev:            devTask,
+//     clean:          cleanTask,
+//     init:           initTask,
+//     sass:           sassTask,
+//     nunjucks:       nunjucksTask,
+//     rollup:         rollupTask,
+//     static:         staticTask,
+//     server:         startServerTask,
+//     watch:          watchTask,
+//     "build:series": buildSeriesTask,
+//     "dev:series":   devSeriesTask,
+// };
+// export default exports;
