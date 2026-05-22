e13a37f9a4a7bd0cab93f7044678daeb7852af2b
Darren Embry <dse@webonastick.com>
2026-05-22 13:18:28 -0400

    diagonal to horizontal and vertical arcs

--- support/bin/puachars.py
+++ support/bin/puachars.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S fontforge -quiet
 # -*- mode: python; coding: utf-8 -*-
 
-import os, argparse
+import os, argparse, math
 sys.path.append("%s/git/dse.d/my-python/src/my_python_dse" % os.getenv("HOME"))
 from font_draw_utils import rect, poly, GA
 from font_utils import get_fonts_from
@@ -253,9 +253,151 @@ def main():
             draw_x_for_hollowed_out_heavy_circle(glyph)
             glyph.removeOverlap()
 
+        for glyph in GA(font, "U+FAF26"):
+            draw_vertical_diagonal_arc(glyph, left=True, upper=True)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF27"):
+            draw_vertical_diagonal_arc(glyph, left=False, upper=True)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF28"):
+            draw_vertical_diagonal_arc(glyph, left=True, upper=False)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF29"):
+            draw_vertical_diagonal_arc(glyph, left=False, upper=False)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF2A"):
+            draw_vertical_diagonal_arc(glyph, left=True, upper=True, horizontal=True)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF2B"):
+            draw_vertical_diagonal_arc(glyph, left=False, upper=True, horizontal=True)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF2C"):
+            draw_vertical_diagonal_arc(glyph, left=True, upper=False, horizontal=True)
+            glyph.removeOverlap()
+        for glyph in GA(font, "U+FAF2D"):
+            draw_vertical_diagonal_arc(glyph, left=False, upper=False, horizontal=True)
+            glyph.removeOverlap()
+
         if filename.endswith(".sfd"):
             font.save(filename)
         else:
             font.generate(filename)
+
+def draw_vertical_diagonal_arc(glyph, clockwise=True, left=True, upper=True, horizontal=False):
+    global STROKE_WIDTH
+
+    font = glyph.font
+
+    this_way = clockwise
+    if not left:
+        this_way = not this_way
+    if not upper:
+        this_way = not this_way
+
+    w = glyph.width
+    h = font.ascent + font.descent
+
+    # lower left corner
+    x0 = 0
+    y0 = -font.descent
+    x0a = x0 - STROKE_WIDTH/2 * h / math.sqrt(w*w + h*h)
+    y0a = y0 + STROKE_WIDTH/2 * w / math.sqrt(w*w + h*h)
+    x0b = x0 + STROKE_WIDTH/2 * h / math.sqrt(w*w + h*h)
+    y0b = y0 - STROKE_WIDTH/2 * w / math.sqrt(w*w + h*h)
+
+    # start of curve
+    x1 = 0
+    y1 = -font.descent
+    if horizontal:
+        x1 = glyph.width / 4
+        y1 = -font.descent + (font.ascent + font.descent) / 4
+    x1a = x1 - STROKE_WIDTH/2 * h / math.sqrt(w*w + h*h)
+    y1a = y1 + STROKE_WIDTH/2 * w / math.sqrt(w*w + h*h)
+    x1b = x1 + STROKE_WIDTH/2 * h / math.sqrt(w*w + h*h)
+    y1b = y1 - STROKE_WIDTH/2 * w / math.sqrt(w*w + h*h)
+
+    # upper center
+    x2 = glyph.width / 2
+    y2 = font.ascent
+    x2a = x2 - STROKE_WIDTH / 2
+    y2a = y2
+    x2b = x2 + STROKE_WIDTH / 2
+    y2b = y2
+
+    if horizontal:
+        x2 = glyph.width
+        y2 = (font.ascent - font.descent) / 2
+        x2a = x2
+        x2b = x2
+        y2a = y2 + STROKE_WIDTH / 2
+        y2b = y2 - STROKE_WIDTH / 2
+
+    # centers used for computing control points
+    xc = glyph.width / 2
+    yc = (font.ascent - font.descent) / 2
+    xca = xc - STROKE_WIDTH/2 * h / math.sqrt(w*w + h*h)
+    xcb = xc + STROKE_WIDTH/2 * h / math.sqrt(w*w + h*h)
+    yca = yc + STROKE_WIDTH/2 * w / math.sqrt(w*w + h*h)
+    ycb = yc - STROKE_WIDTH/2 * w / math.sqrt(w*w + h*h)
+
+    # control points
+    x3a = x1a + (xca - x1a) * C
+    y3a = y1a + (yca - y1a) * C
+    x3b = x1b + (xcb - x1b) * C
+    y3b = y1b + (ycb - y1b) * C
+    x4a = x2a
+    x4b = x2b
+    y4a = y2a + (yca - y2a) * C
+    y4b = y2b + (ycb - y2b) * C
+    if horizontal:
+        x4a = glyph.width + (xca - glyph.width) * C
+        x4b = glyph.width + (xcb - glyph.width) * C
+        y4a = y2a
+        y4b = y2b
+
+    if not left:
+        (x0a, x0b, x1a, x3a, x4a, x2a, x2b, x4b, x3b, x1b) = [
+            glyph.width - x
+            for x in (x0a, x0b, x1a, x3a, x4a, x2a, x2b, x4b, x3b, x1b)
+        ]
+    if not upper:
+        (y0a, y0b, y1a, y3a, y4a, y2a, y2b, y4b, y3b, y1b) = [
+            font.ascent - font.descent - y
+            for y in (y0a, y0b, y1a, y3a, y4a, y2a, y2b, y4b, y3b, y1b)
+        ]
+
+    pen = glyph.glyphPen(replace=False)
+    if this_way:
+        if horizontal:
+            pen.moveTo((x0a, y0a))
+            pen.lineTo((x1a, y1a))
+            pen.curveTo((x3a, y3a), (x4a, y4a), (x2a, y2a))
+            pen.lineTo((x2b, y2b))
+            pen.curveTo((x4b, y4b), (x3b, y3b), (x1b, y1b))
+            pen.lineTo((x0b, y0b))
+            pen.lineTo((x0a, y0a))
+        else:
+            pen.moveTo((x1a, y1a))
+            pen.curveTo((x3a, y3a), (x4a, y4a), (x2a, y2a))
+            pen.lineTo((x2b, y2b))
+            pen.curveTo((x4b, y4b), (x3b, y3b), (x1b, y1b))
+            pen.lineTo((x1a, y1a))
+    else:
+        if horizontal:
+            pen.moveTo((x0a, y0a))
+            pen.lineTo((x0b, y0b))
+            pen.lineTo((x1b, y1b))
+            pen.curveTo((x3b, y3b), (x4b, y4b), (x2b, y2b))
+            pen.lineTo((x2a, y2a))
+            pen.curveTo((x4a, y4a), (x3a, y3a), (x1a, y1a))
+            pen.lineTo((x0a, y0a))
+        else:
+            pen.moveTo((x1a, y1a))
+            pen.lineTo((x1b, y1b))
+            pen.curveTo((x3b, y3b), (x4b, y4b), (x2b, y2b))
+            pen.lineTo((x2a, y2a))
+            pen.curveTo((x4a, y4a), (x3a, y3a), (x1a, y1a))
+    pen.closePath()
+    pen = None
 
 main()
