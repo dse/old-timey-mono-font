968a70c6ffe7d85e25a40f045765309be8734e60
Darren Embry <dse@webonastick.com>
2026-06-29 19:24:29 -0400

    bounds.py has no reason to save the font

--- support/bin/bounds.py
+++ support/bin/bounds.py
@@ -10,34 +10,23 @@ sys.path.append(os.path.dirname(__file__) + "/../lib")
 from my_font_utils import check_all_glyph_bounds
 
 DEFAULT_WIDTH = 1008
-LATIN_SMALL_LETTER_SCHWA = 0x0259
 
 def main():
     global args
     parser = argparse.ArgumentParser()
     parser.add_argument('font_filename', help="font filename")
     parser.add_argument('-w', '--width', type=int, default=DEFAULT_WIDTH)
-    parser.add_argument('-o', '--save-as', '--output', type=str)
     parser.add_argument('--verbose', '-v', action='count', default=0)
     args = parser.parse_args()
 
-    if "DEBUG" in os.environ:
-        print("bounds.py %s: Opening and reading..." % args.font_filename)
+    if args.verbose:
+        print("bounds.py: %s: Opening and reading..." % args.font_filename)
     font = fontforge.open(args.font_filename)
-    write_font_filename = args.save_as if args.save_as is not None else args.font_filename
 
-    if "DEBUG" in os.environ:
-        print("bounds.py %s: Checking glyph bounds..." % args.font_filename)
+    if args.verbose:
+        print("bounds.py: %s: Checking glyph bounds..." % args.font_filename)
     check_all_glyph_bounds(font, args.width)
 
-    if write_font_filename.endswith('.sfd'):
-        if "DEBUG" in os.environ:
-            print("bounds.py %s: Saving %s..." % (args.font_filename, write_font_filename))
-        font.save(write_font_filename)
-    else:
-        if "DEBUG" in os.environ:
-            print("bounds.py %s: Generating %s..." % (args.font_filename, write_font_filename))
-        font.generate(write_font_filename)
     font.close()
 
 main()
