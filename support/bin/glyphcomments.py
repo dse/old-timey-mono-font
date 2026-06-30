6a088bc6b116e9ba6e4caf86c9ab11968509f562
Darren Embry <dse@webonastick.com>
2026-06-30 01:03:52 -0400

    glyphcomments.py script

---
+++ support/bin/glyphcomments.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S fontforge -quiet -lang=py -script
+import fontforge, argparse, re
+
+def main():
+    global args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("--erase", action="store_true")
+    args = parser.parse_args()
+
+    font = fontforge.open(args.filename)
+    for glyph in font.glyphs():
+        if args.erase:
+            glyph.comment = ""
+        else:
+            if glyph.comment is not None and glyph.comment.strip() != "":
+                print("%s:" % glyph.glyphname)
+                print(re.sub(r'^', "> ", glyph.comment, flags=re.M))
+
+    if args.filename.endswith(".sfd"):
+        font.save(args.filename)
+    else:
+        font.generate(args.filename)
+    font.close()
+
+main()
