294751e19f583ca3af967f41a5de117d826572f1
Darren Embry <dse@webonastick.com>
2026-06-30 17:44:18 -0400

    corrections for "aring" and "hbar"

--- support/bin/italicadjust.py
+++ support/bin/italicadjust.py
@@ -24,6 +24,11 @@ def main():
     for glyph in font.glyphs():
         italic_shift_type = compute_italic_shift_type(glyph)
         if italic_shift_type is not None:
+            if args.verbose:
+                print("%s: %s (%s): italic shift for %s case letters" % (args.filename,
+                                                                         glyph.glyphname,
+                                                                         u(glyph.unicode),
+                                                                         italic_shift_type))
             do_italic_shift(glyph, italic_shift_type)
 
     if args.filename.endswith(".sfd"):
@@ -34,7 +39,8 @@ def main():
     font.close()
 
 def compute_italic_shift_type(glyph):
-    if glyph.comment == "italicized":
+    global args
+    if is_italicized(glyph):
         if glyph.temporary["cat"] in ["Ll"]:
             return "lower"
         elif glyph.temporary["cat"] in ["Lu"]:
@@ -53,7 +59,13 @@ def compute_italic_shift_type(glyph):
     else:                       # either uppercase only or mixed upper/lowercase
         return "upper"
 
+def is_italicized(glyph):
+    return glyph.comment == "italicized" or any([
+        is_italicized(glyph.font[ref[0]]) for ref in glyph.references
+    ])
+
 def do_italic_shift(glyph, italic_shift_type):
+    global args
     if italic_shift_type == "lower":
         base_y_center = glyph.font.xHeight / 2
     else:
@@ -61,12 +73,29 @@ def do_italic_shift(glyph, italic_shift_type):
     if not len(glyph.references):
         return
     new_refs = []
+
+    ref_glyphs = [glyph.font[ref[0]] for ref in glyph.references]
+    ref_base_glyphs = [glyph for glyph in ref_glyphs if is_italicizable_base(glyph)]
+    ref_base_glyph_names = [glyph.glyphname for glyph in ref_base_glyphs]
+
+    correction = 0
+    if ref_base_glyph_names == ["a"]:
+        correction = 80
+
     for ref in glyph.references:
         ref_glyph = glyph.font[ref[0]]
         if is_italicizable_mark(ref_glyph):
             (_, y_min, _, y_max) = ref_glyph.boundingBox()
             mark_y_center = (y_min + y_max) / 2
-            shift_x = (mark_y_center - base_y_center) * math.tan(-glyph.font.italicangle * math.pi / 180)
+            if args.verbose:
+                print("    %s (%s) is %d milliems above the appropriate height" % (ref_glyph.glyphname,
+                                                                                   u(ref_glyph.unicode),
+                                                                                   abs(mark_y_center - base_y_center)))
+            shift_x = (mark_y_center - base_y_center) * math.tan(-glyph.font.italicangle * math.pi / 180) + correction
+            if args.verbose:
+                print("    shifting %s (%s) %d milliems to the right" % (ref_glyph.glyphname,
+                                                                         u(ref_glyph.unicode),
+                                                                         shift_x))
             transform = psMat.compose(ref[1], psMat.translate(shift_x, 0))
             ref = list(ref)
             ref[1] = transform
@@ -76,7 +105,8 @@ def do_italic_shift(glyph, italic_shift_type):
     glyph.references = tuple(new_refs)
 
 def is_italicizable_mark(glyph):
-    return glyph.temporary["norm_codepoint"] in range(0x0300, 0x0370)
+    return glyph.temporary["norm_codepoint"] in range(0x0300, 0x0370) or \
+        glyph.glyphname == "x_mediumhorizline"
 
 def is_italicizable_base(glyph):
     return glyph.temporary["cat"] in ["Ll", "Lu"]
