b5ea5c08273f92b4f5f528833a1836e48137311f
Darren Embry <dse@webonastick.com>
2026-09-18 19:14:28 -0400

    underline.py now takes -v.

--- support/bin/underline.py
+++ support/bin/underline.py
@@ -7,21 +7,22 @@ def main():
     parser.add_argument('center', type=int)
     parser.add_argument('width', type=int)
     parser.add_argument('filenames', nargs='+')
+    parser.add_argument('--verbose', '-v', action='count', default=0)
     args = parser.parse_args()
     for filename in args.filenames:
-        if "DEBUG" in os.environ:
+        if args.verbose:
             print("underline.py %s: Opening and reading..." % filename)
         font = fontforge.open(filename)
         font.upos = int(args.center - args.width / 2)
         font.uwidth = args.width
-        if "DEBUG" in os.environ:
+        if args.verbose:
             print("underline.py %s: position = %d; width = %d" % (filename, font.upos, font.uwidth))
         if filename.endswith('.sfd'):
-            if "DEBUG" in os.environ:
+            if args.verbose:
                 print("underline.py %s: Saving..." % filename)
             font.save(filename)
         else:
-            if "DEBUG" in os.environ:
+            if args.verbose:
                 print("underline.py %s: Generating..." % filename)
             font.generate(filename)
         font.close()
